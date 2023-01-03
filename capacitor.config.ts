de170e8de26015fc41c61bb0fb382f53a681c63c
Bogdan_Golota <bogdan.golota@code-seek.com>
2023-01-03 17:40:02 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pocket',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
